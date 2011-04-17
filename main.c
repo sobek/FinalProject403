b857070910a6c7b38e1f3c196cff697879993753
Justin Nichols <jrn@umail.iu.edu>
2011-04-17 13:42:37 -0400

    Reading file in, it works and I want to be able to reference this point.

--- main.c
+++ main.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+//prototypes
+//pass an address location?
+void my_read_file(char *file_address_from_argv);
+
+
+int main(int argc, char *argv[])
 {
-    printf("Hello world!\n");
-    //commenting somethin
-    //adding comment from my laptop now.
-    //again
+
+    //my_read_file(argv[1]);
+readinput:
+    if(argv[1] == NULL)
+    {
+        printf("Please enter a input file.");
+        goto readinput;
+    }else
+    {
+        my_read_file(argv[1]);
+    }
+
     return 0;
+}
+
+
+void my_read_file(char *file_address_from_argv)
+{
+    printf("%s should be the input.", file_address_from_argv);
 }
