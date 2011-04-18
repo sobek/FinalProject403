5127ed7070f4168fe809eb6ea204982cfaae57b5
Justin Nichols <jrn@umail.iu.edu>
2011-04-17 21:54:02 -0400

    File is read into a character array in memory seemingly successfully.

--- main.c
+++ main.c
@@ -29,36 +29,35 @@ char *edge;
 int main(int argc, char *argv[])
 {
 
-    //my_read_file(argv[1]);
-
-    if(argv[1] == NULL)
-    {
-        printf("Please run again using the format: \"./a.out /path/to/input/file\" where \"./a.out\" is the name of executable\n");
-        return -1;
-
-    }else
-    {
-        my_read_file(argv[1]);
-    }
+    my_read_file(argv[1]);
 
     return 0;
 }
 
-//this needs to process the file into usable pieces
+//reads the file into here.  this file breaks it down into the nodes and edges I need.
 void my_read_file(char *file_address_from_argv)
 {
-    printf("Hello in fn!\n");
+    int c;
+    long file_size;
+    char *array_for_file;
     FILE *input_file;
     input_file = fopen(file_address_from_argv, "r");
-    int c;
-
-    while(c != EOF)
+    if((input_file) == NULL)
     {
-    c = fgetc(input_file);
-    putchar(c);
+        printf("Please run again using the format: \"./a.out /path/to/input/file\" where \"./a.out\" is the name of executable, and the path is valid\n");
+        exit(-1);
     }
 
+    //read file into an array in memory
+    size_t result;
+    fseek(input_file, 0, SEEK_END);
+    file_size = ftell(input_file);
+    fseek(input_file, 0, SEEK_SET);
+    array_for_file = (char*)malloc(sizeof(char)*file_size);
+    fread(array_for_file,1,file_size,input_file);
 
-
+    //close the file, should be in mem at this point.
     fclose(input_file);
+
+    printf("%s", array_for_file);
 }
